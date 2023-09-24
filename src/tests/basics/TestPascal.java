128e6b37df0f1fbc6a303ac0048bdba141843707
emec delam <kiwi.bp.surviv@gmail.com>
2023-09-24 19:16:06 +0200

    added Recursive pascal, not working on inginious somehow, maybe due to time limit

---
+++ src/tests/basics/TestPascal.java
@@ -0,0 +1,15 @@
+package tests.basics;
+
+import basics.Pascal;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestPascal {
+    @Test
+    public void testPascal(){
+        int[] actual = Pascal.pascal(5);
+        int[] expected = new int[]{1, 4, 6, 4, 1};
+        Assertions.assertEquals(expected.length, actual.length);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}

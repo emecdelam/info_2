9b5f716d71087c5cbd0862911cbeff20ea0795a9
emec delam <kiwi.bp.surviv@gmail.com>
2023-09-24 13:37:08 +0200

    adding magic square

--- src/tests/parallelization/TestFindInMatrix.java
+++
@@ -1,9 +0,0 @@
-package tests.parallelization;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
-public class TestFindInMatrix {
-    @Test
-    public void testResult(){
-        int x = 0;
-    }
-}

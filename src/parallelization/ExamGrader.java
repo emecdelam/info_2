47076db4a077b6cd3b7c0182b6ef8d12f47a525a
Emec Delam <kiwi.bp.surviv@gmail.com>
2023-10-03 20:27:01 +0200

    adding ExamGrader solutions

---
+++ src/parallelization/ExamGrader.java
@@ -0,0 +1,85 @@
+package parallelization;// You can add imports
+
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ExamGrader {
+
+    // You can add new methods, inner classes, etc.
+
+    public interface RoundingFunction {
+        int roundGrade(double grade);
+    }
+
+
+    public static class ExamQuestion {
+        public double pointsObtained;
+        public ExamQuestion nextQuestion;
+
+        public ExamQuestion(double points, ExamQuestion next) {
+            this.pointsObtained = points;
+            this.nextQuestion = next;
+        }
+
+    }
+
+    /**
+     *  Write a method calculateExamGrade that calculates the grade
+     *  obtained for an exam. The grade is the sum of the points
+     *  obtained in all questions. The questions are provided in a
+     *  linked list (class ExamQuestion).
+     *
+     *  However, the points are real numbers, while the exam grade
+     *  is a natural number. To round the grade, the caller of this
+     *  method provides a rounding function that you have to use
+     *  to obtain the final result.
+     *
+     *  Look at the test "testWithTwoQuestions" to see how users will
+     *  call this method.
+     *
+     *  You can assume that all points are positive numbers and that
+     *  the list of questions is not empty.
+     */
+    public static int calculateExamGrade(ExamQuestion questions, RoundingFunction roundingFunction) {
+        int sum = 0;
+        ExamQuestion current = questions;
+        while (current.nextQuestion != null){
+            sum += roundingFunction.roundGrade(current.pointsObtained);
+            current = current.nextQuestion;
+        }
+        sum += roundingFunction.roundGrade(current.pointsObtained);
+        return sum;
+    }
+
+
+    /**
+     * Write a method gradeExams that calculates the grades of two exams
+     * using two threads to accelerate the grading (one exam graded per thread).
+     * The method must return the two grades in an int array.
+     * Like for the method calculateExamGrade, the caller of this method
+     * provides a rounding function.
+     *
+     * Look at the test "testTwoShortExams" to see how users will
+     * call this method.
+     *
+     * You MUST use threads (or a threadpool).
+     * Catch (and ignore) all exceptions.
+     */
+    public static int[] gradeExams(ExamQuestion exam1, ExamQuestion exam2, RoundingFunction roundingFunction) {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        Future<Integer> future1 = executorService.submit(()->calculateExamGrade(exam1,roundingFunction));
+        Future<Integer> future2 = executorService.submit(()->calculateExamGrade(exam2,roundingFunction));
+        int[] list = new int[2];
+        try {
+            list[0] = future1.get();
+            list[1] = future2.get();
+
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return list;
+    }
+}
